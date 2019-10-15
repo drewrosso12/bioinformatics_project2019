f6ecb1bfb1b520c0df640d33c2e955df0fedcf0e
TessMarvin <tmarvin@nd.edu>
2019-10-14 23:51:11 -0400

    updated this first draft of the dual sh

--- methaneph.sh
+++ methaneph.sh
@@ -18,15 +18,16 @@ cat $3 >> hsp70.refs
 for files in $2
 do
   	recordName=$(echo $files | sed 's/.fasta//g')
-        ./hmmer/bin/hmmsearch buildmcrA.hmm $files | head -n 20 > $recordNamemmcrA.txt
-	./hmmer/bin/hmmsearch buildhsp70.hmm $files | head -n 15 > $recordNamehsp70.txt
+        ./hmmer/bin/hmmsearch buildmcrA.hmm $files >  $recordName"m".txt
+	./hmmer/bin/hmmsearch buildhsp70.hmm $files > $recordName"h".txt
 
-       #proteome=$(echo $recordName | sed 's/.\/proteomes\///g')
+       proteome=$(echo $recordName | sed 's/.\/proteomes\///g')
         #create summary table collating results -- if there is an mcrA gene then it will give the coenzyme name -- if not it indicates there were no hits for that proteome
-       # echo -n  $proteome " "
-       # cat $recordName.txt | egrep -o -m 1 "(coenzyme-B sulfoethylthiotransferase subunit alpha |\[No hits detected that satisfy reporting thresholds\])"
-       # echo -n  $proteome " " >> summary.txt
-#cat $recordName.txt | egrep -o -m 1 "(coenzyme-B sulfoethylthiotransferase subunit alpha |\[No hits detected that satisfy reporting thresholds\])" >>summary.txt
+        echo -n  $proteome " "
+        cat $recordName"m".txt | egrep -o -m 1 "(coenzyme-B sulfoethylthiotransferase subunit alpha |\[No hits detected that satisfy reporting thresholds\])"
+        #cat $recordName"m".txt | egrep -o -m 1 "(coenzyme-B sulfoethylthiotransferase subunit alpha |\[No hits detected that satisfy reporting thresholds\])"
+	echo -n  $proteome " " >> summary.txt
+cat $recordName.txt | egrep -o -m 1 "(coenzyme-B sulfoethylthiotransferase subunit alpha |\[No hits detected that satisfy reporting thresholds\])" >>summary.txt
 done
 #takes only those proteomes with mcrA gene and creates txt file with the name of associated proteome
 #cat summary.txt | egrep "coenzyme-B sulfoethylthiotransferase subunit alpha" |cut -d ' ' -f 1,1 >> namesofsuitableproteomes.txt
