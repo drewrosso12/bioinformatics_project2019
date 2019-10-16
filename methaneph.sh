f20aa8e2d6863a73fb9a54906aafc2df74bb4277
TessMarvin <tmarvin@nd.edu>
2019-10-15 21:12:40 -0400

    here is what I think the list of proteomes is

--- methaneph.sh
+++ methaneph.sh
@@ -21,13 +21,16 @@ do
         ./hmmer/bin/hmmsearch buildmcrA.hmm $files >  $recordName"m".txt
 	./hmmer/bin/hmmsearch buildhsp70.hmm $files > $recordName"h".txt
 
-       proteome=$(echo $recordName | sed 's/.\/proteomes\///g')
-        #create summary table collating results -- if there is an mcrA gene then it will give the coenzyme name -- if not it indicates there were no hits for that proteome
-        echo -n  $proteome " "
-        cat $recordName"m".txt | egrep -o -m 1 "(coenzyme-B sulfoethylthiotransferase subunit alpha |\[No hits detected that satisfy reporting thresholds\])"
-        #cat $recordName"m".txt | egrep -o -m 1 "(coenzyme-B sulfoethylthiotransferase subunit alpha |\[No hits detected that satisfy reporting thresholds\])"
-	echo -n  $proteome " " >> summary.txt
-cat $recordName.txt | egrep -o -m 1 "(coenzyme-B sulfoethylthiotransferase subunit alpha |\[No hits detected that satisfy reporting thresholds\])" >>summary.txt
+      proteome=$(echo $recordName | sed 's/.\/proteomes\///g')
+      create summary table collating results -- if there is an mcrA gene then it will give the coenzyme name -- if not it indicates there were no hits for that proteome
+      echo $proteome " "
+      cat $recordName"m".txt
+      cat $recordName"h".txt 
+      echo -n  $proteome " " >> summary.txt
+mcrA=$(cat $recordName"m".txt | egrep -o -m 1 "(coenzyme-B sulfoethylthiotransferase subunit alpha |\[No hits detected that satisfy reporting thresholds\])")
+hsp70=$(cat $recordName"h".txt | egrep -o -m 1 "(Alignments for each domain|\[No hits detected that satisfy reporting thresholds\])")
+echo -n  $mcrA " " >> summary.txt
+echo $hsp70 >> summary.txt
 done
 #takes only those proteomes with mcrA gene and creates txt file with the name of associated proteome
-#cat summary.txt | egrep "coenzyme-B sulfoethylthiotransferase subunit alpha" |cut -d ' ' -f 1,1 >> namesofsuitableproteomes.txt
+cat summary.txt | egrep "(coenzyme-B sulfoethylthiotransferase subunit alpha.*Alignments for each domain)" |cut -d ' ' -f 1,1 >> namesofsuitableproteomes.txt
